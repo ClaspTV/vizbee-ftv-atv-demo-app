fcf4c3e707d7e7aca6dd16977b7b6b3f7d0aff72
sreenu cheedella <sreenu@vizbee.tv>
2024-09-18 21:26:20 +0530

    Fixed player card dismiss and adjusted the main activity layout

--- app/src/main/java/tv/vizbee/screendemo/ui/video/ExoPlayerActivity.kt
+++ app/src/main/java/tv/vizbee/screendemo/ui/video/ExoPlayerActivity.kt
@@ -136,7 +136,7 @@ class ExoPlayerActivity : AppCompatActivity(), MediaSourceEventListener, Player.
                 .build()
             mediaSession?.setPlaybackState(playbackState)
         }
-            ?: kotlin.run { applicationContext.vizbeeAppLifecycleAdapter?.getAppReadyModel()?.deeplinkManager?.handleDeeplinkFailure() }
+//            ?: kotlin.run { applicationContext.vizbeeAppLifecycleAdapter?.getAppReadyModel()?.deeplinkManager?.handleDeeplinkFailure() }
     }
 
     private fun updateMediaMetadata() {
