4b64367f1b0be037921bc22d75c429f38b525758
Radhakrishna <radhakrishna@vizbee.tv>
2024-09-19 17:55:19 +0530

    homesso:fix isSignedIn impl, add logging, clean up unnecessary setSignedIn maintainance in AuthManager

--- app/src/main/java/tv/vizbee/screendemo/auth/AuthManager.kt
+++ app/src/main/java/tv/vizbee/screendemo/auth/AuthManager.kt
@@ -1,24 +1,13 @@
 package tv.vizbee.screendemo.auth
 
 import android.content.Context
-import android.content.SharedPreferences
-import androidx.core.content.edit
-import tv.vizbee.screendemo.vizbee.homesso.MyVizbeeHomeSSOAdapter
+import tv.vizbee.screendemo.storage.SharedPreferencesManager
 
 class AuthManager(context: Context) {
-    private val prefs: SharedPreferences = context.getSharedPreferences("AuthPrefs", Context.MODE_PRIVATE)
-
-    fun setSignedIn(signInType: String, isSignedIn: Boolean) {
-        prefs.edit {
-            putBoolean(signInType, isSignedIn)
-        }
-    }
+    private val sharedPreferencesManager = SharedPreferencesManager(context)
 
     fun isSignedIn(signInType: String): Boolean {
-        return prefs.getBoolean(signInType, false)
-    }
-
-    fun isSignedInAny(): Boolean {
-        return MyVizbeeHomeSSOAdapter.SUPPORTED_SIGN_IN_TYPES.any { isSignedIn(it) }
+        val authToken = sharedPreferencesManager.getStringValue("authToken")
+        return authToken.isNotEmpty()
     }
 }
